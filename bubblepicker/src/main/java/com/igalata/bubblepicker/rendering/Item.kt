fd0d8584ede32e98e0b1174a63943e9e03addcdd
Truong Khanh <khanhth@dwarvesv.com>
2020-10-29 18:14:32 +0700

    feat: update bubble drawer

--- bubblepicker/src/main/java/com/igalata/bubblepicker/rendering/Item.kt
+++ bubblepicker/src/main/java/com/igalata/bubblepicker/rendering/Item.kt
@@ -1,12 +1,12 @@
 package com.igalata.bubblepicker.rendering
 
 import android.graphics.*
-import android.graphics.drawable.BitmapDrawable
 import android.opengl.GLES20.*
 import android.opengl.Matrix
 import android.text.Layout
 import android.text.StaticLayout
 import android.text.TextPaint
+import android.text.TextUtils
 import com.igalata.bubblepicker.model.BubbleGradient
 import com.igalata.bubblepicker.model.PickerItem
 import com.igalata.bubblepicker.physics.CircleBody
@@ -17,7 +17,7 @@ import org.jbox2d.common.Vec2
 /**
  * Created by irinagalata on 1/19/17.
  */
-data class Item(val pickerItem: PickerItem, val circleBody: CircleBody) {
+data class Item(var pickerItem: PickerItem, val circleBody: CircleBody) {
 
     val x: Float
         get() = circleBody.physicalBody.position.x
@@ -37,9 +37,6 @@ data class Item(val pickerItem: PickerItem, val circleBody: CircleBody) {
     private var isVisible = true
         get() = circleBody.isVisible
     private var texture: Int = 0
-    private var imageTexture: Int = 0
-    private val currentTexture: Int
-        get() = if (circleBody.increased || circleBody.isIncreasing) imageTexture else texture
     private val bitmapSize = 256f
     private val gradient: LinearGradient?
         get() {
@@ -55,7 +52,7 @@ data class Item(val pickerItem: PickerItem, val circleBody: CircleBody) {
 
     fun drawItself(programId: Int, index: Int, scaleX: Float, scaleY: Float) {
         glActiveTexture(GL_TEXTURE)
-        glBindTexture(GL_TEXTURE_2D, currentTexture)
+        glBindTexture(GL_TEXTURE_2D, texture)
         glUniform1i(glGetUniformLocation(programId, BubbleShader.U_TEXT), 0)
         glUniform1i(glGetUniformLocation(programId, BubbleShader.U_VISIBILITY), if (isVisible) 1 else -1)
         glUniformMatrix4fv(glGetUniformLocation(programId, U_MATRIX), 1, false, calculateMatrix(scaleX, scaleY), 0)
@@ -63,36 +60,35 @@ data class Item(val pickerItem: PickerItem, val circleBody: CircleBody) {
     }
 
     fun bindTextures(textureIds: IntArray, index: Int) {
-        texture = bindTexture(textureIds, index * 2, false)
-        imageTexture = bindTexture(textureIds, index * 2 + 1, true)
+        texture = bindTexture(textureIds, index)
     }
 
-    private fun createBitmap(isSelected: Boolean): Bitmap {
+    fun createBitmap(): Bitmap {
         var bitmap = Bitmap.createBitmap(bitmapSize.toInt(), bitmapSize.toInt(), Bitmap.Config.ARGB_4444)
         val bitmapConfig: Bitmap.Config = bitmap.config ?: Bitmap.Config.ARGB_8888
         bitmap = bitmap.copy(bitmapConfig, true)
 
         val canvas = Canvas(bitmap)
 
-        if (isSelected) drawImage(canvas)
-        drawBackground(canvas, isSelected)
+        drawBackground(canvas)
         drawIcon(canvas)
         drawText(canvas)
 
         return bitmap
     }
 
-    private fun drawBackground(canvas: Canvas, withImage: Boolean) {
+    private fun drawBackground(canvas: Canvas) {
         val bgPaint = Paint()
         bgPaint.style = Paint.Style.FILL
         pickerItem.color?.let { bgPaint.color = pickerItem.color!! }
         pickerItem.gradient?.let { bgPaint.shader = gradient }
-        if (withImage) bgPaint.alpha = (pickerItem.overlayAlpha * 255).toInt()
         canvas.drawRect(0f, 0f, bitmapSize, bitmapSize, bgPaint)
     }
 
     private fun drawText(canvas: Canvas) {
         if (pickerItem.title == null || pickerItem.textColor == null) return
+
+        var title = pickerItem.title!!
 
         val paint = TextPaint(Paint.ANTI_ALIAS_FLAG).apply {
             color = pickerItem.textColor!!
@@ -100,29 +96,31 @@ data class Item(val pickerItem: PickerItem, val circleBody: CircleBody) {
             typeface = pickerItem.typeface
         }
 
-        val maxTextHeight = if (pickerItem.icon == null) bitmapSize / 2f else bitmapSize / 2.7f
+        var textLayout = placeText(paint, title)
 
-        var textLayout = placeText(paint)
-
-        while (textLayout.height > maxTextHeight) {
-            paint.textSize--
-            textLayout = placeText(paint)
+        when {
+            pickerItem.icon == null -> {
+                canvas.translate((bitmapSize - textLayout.width) / 2f, (bitmapSize - textLayout.height) / 2f)
+            }
+            pickerItem.iconOnTop -> {
+                canvas.translate((bitmapSize - textLayout.width) / 2f, bitmapSize / 2f)
+            }
+            else -> {
+                canvas.translate((bitmapSize - textLayout.width) / 2f, bitmapSize / 2 - textLayout.height)
+            }
         }
-
-        if (pickerItem.icon == null) {
-            canvas.translate((bitmapSize - textLayout.width) / 2f, (bitmapSize - textLayout.height) / 2f)
-        } else if (pickerItem.iconOnTop) {
-            canvas.translate((bitmapSize - textLayout.width) / 2f, bitmapSize / 2f)
-        } else {
-            canvas.translate((bitmapSize - textLayout.width) / 2f, bitmapSize / 2 - textLayout.height)
-        }
-
         textLayout.draw(canvas)
     }
 
-    private fun placeText(paint: TextPaint): StaticLayout {
-        return StaticLayout(pickerItem.title, paint, (bitmapSize * 0.9).toInt(),
-                Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false)
+    private fun placeText(paint: TextPaint, text: String): StaticLayout {
+        val builder = StaticLayout.Builder.obtain(text, 0, text.length, paint, (bitmapSize * 0.85).toInt())
+                .setAlignment(Layout.Alignment.ALIGN_CENTER)
+                .setMaxLines(3)
+                .setEllipsize(TextUtils.TruncateAt.END)
+                .setIncludePad(false)
+                .setLineSpacing(0.0f, 1.0f)
+
+        return builder.build()
     }
 
     private fun drawIcon(canvas: Canvas) {
@@ -133,33 +131,25 @@ data class Item(val pickerItem: PickerItem, val circleBody: CircleBody) {
             val left = (bitmapSize / 2 - width / 2).toInt()
             val right = (bitmapSize / 2 + width / 2).toInt()
 
-            if (pickerItem.title == null) {
-                it.bounds = Rect(left, (bitmapSize / 2 - height / 2).toInt(), right, (bitmapSize / 2 + height / 2).toInt())
-            } else if (pickerItem.iconOnTop) {
-                it.bounds = Rect(left, (bitmapSize / 2 - height).toInt(), right, (bitmapSize / 2).toInt())
-            } else {
-                it.bounds = Rect(left, (bitmapSize / 2).toInt(), right, (bitmapSize / 2 + height).toInt())
+            when {
+                pickerItem.title == null -> {
+                    it.bounds = Rect(left, (bitmapSize / 2 - height / 2).toInt(), right, (bitmapSize / 2 + height / 2).toInt())
+                }
+                pickerItem.iconOnTop -> {
+                    it.bounds = Rect(left, (bitmapSize / 2 - height).toInt(), right, (bitmapSize / 2).toInt())
+                }
+                else -> {
+                    it.bounds = Rect(left, (bitmapSize / 2).toInt(), right, (bitmapSize / 2 + height).toInt())
+                }
             }
 
             it.draw(canvas)
         }
     }
 
-    private fun drawImage(canvas: Canvas) {
-        pickerItem.backgroundImage?.let {
-            val height = (it as BitmapDrawable).bitmap.height.toFloat()
-            val width = it.bitmap.width.toFloat()
-            val ratio = Math.max(height, width) / Math.min(height, width)
-            val bitmapHeight = if (height < width) bitmapSize else bitmapSize * ratio
-            val bitmapWidth = if (height < width) bitmapSize * ratio else bitmapSize
-            it.bounds = Rect(0, 0, bitmapWidth.toInt(), bitmapHeight.toInt())
-            it.draw(canvas)
-        }
-    }
-
-    private fun bindTexture(textureIds: IntArray, index: Int, withImage: Boolean): Int {
+    private fun bindTexture(textureIds: IntArray, index: Int): Int {
         glGenTextures(1, textureIds, index)
-        createBitmap(withImage).toTexture(textureIds[index])
+        createBitmap().toTexture(textureIds[index])
         return textureIds[index]
     }
 
