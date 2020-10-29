166fc209d7a917d930a0b6a38dce0d6dd6d56b6a
Truong Khanh <khanhth@dwarvesv.com>
2020-10-29 18:03:09 +0700

    feat: add new adapter

--- bubblepicker/src/main/java/com/igalata/bubblepicker/adapter/BubblePickerAdapter.kt
+++ bubblepicker/src/main/java/com/igalata/bubblepicker/adapter/BubblePickerAdapter.kt
@@ -12,3 +12,24 @@ interface BubblePickerAdapter {
     fun getItem(position: Int): PickerItem
 
 }
+
+abstract class BubbleAdapter() {
+    abstract var totalItem: Int
+    abstract fun getItem(position: Int): PickerItem
+    abstract fun onClick(x: Float, y: Float, itemData: PickerItem)
+
+    // Listener
+    lateinit var listener: InternalListener
+    interface InternalListener {
+        fun scalingItem(position: Int, newRadius: Float)
+        fun setNewItems(updateItems: ArrayList<PickerItem>, removeItems: ArrayList<PickerItem>, addItems: ArrayList<PickerItem>)
+    }
+
+    fun setNewItems(updateItems: ArrayList<PickerItem>, removeItems: ArrayList<PickerItem>, addItems: ArrayList<PickerItem>) {
+        listener.setNewItems(updateItems, removeItems, addItems)
+    }
+
+    fun scalingItem(position:Int, newRadius: Float) {
+        listener.scalingItem(position, newRadius)
+    }
+}
